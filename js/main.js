6ffa2d4e697aec4596f84fc0e5a78e3bb45bb90d
Diego Martin Ruiz <ruizdiegomartin.coder@gmail.com>
2022-09-16 09:57:35 -0300

    Primer etapa terminada del simulador de Carrito

--- js/main.js
+++ js/main.js
@@ -1,56 +1,33 @@
 
-//FUNCION CONSTRUCTORA
+// FUNCION CONSTRUCTORA
 
-function Product (id, img, product, price, category, stock) {
+function Product (id, img, product, price, category, stock, amount) {
   this.id = id;
   this.img = img;
   this.name = product;
   this.price = price;
   this.category = category;
   this.stock = stock;
+  this.amount = amount;
 }
 
 // PRODUCTS
 
-const product1 = new Product (1,"./img/product_soy-candle.jpg", "Vela de soja", 600, "velas", 20);
-const product2 = new Product (2,"./img/product_difusor.jpg", "Difusor aromático", 500, "difusores", 35);
-const product3 = new Product (3,"./img/product-bolsa-aromatizadora.JPG", "Bolsa aromática", 300, "difusores", 15);
-const product4 = new Product (4,"./img/product_splash.jpg", "Splash difusor", 700, "difusores", 25);
-const product5 = new Product (5,"./img/product_liquid-soap.jpg", "Jabón líquido", 650, "limpieza", 23);
-const product6 = new Product (6,"./img/product_nordic-blanket.jpg", "Manta nórdica", 5000, "otros", 10);
-const product7 = new Product (7,"./img/product-aromatizador-auto.JPG", "Difusor de auto", 450, "difusores", 15);
-const product8 = new Product (8,"./img/vela-silver.png", "Vela silver", 750, "velas", 20);
-const product9 = new Product (9,"./img/product_bubble-candle.jpg", "Vela burbuja", 700, "velas", 22);
+const product1 = new Product (1,"./img/product_soy-candle.jpg", "Vela de soja", 600, "velas", 20, 1);
+const product2 = new Product (2,"./img/product_difusor.jpg", "Difusor aromático", 500, "difusores", 35, 1);
+const product3 = new Product (3,"./img/product-bolsa-aromatizadora.JPG", "Bolsa aromática", 300, "difusores", 15, 1);
+const product4 = new Product (4,"./img/product_splash.jpg", "Splash difusor", 700, "difusores", 25, 1);
+const product5 = new Product (5,"./img/product_liquid-soap.jpg", "Jabón líquido", 650, "limpieza", 23, 1);
+const product6 = new Product (6,"./img/product_nordic-blanket.jpg", "Manta nórdica", 5000, "otros", 10, 1);
+const product7 = new Product (7,"./img/product-aromatizador-auto.JPG", "Difusor de auto", 450, "difusores", 15, 1);
+const product8 = new Product (8,"./img/vela-silver.png", "Vela silver", 750, "velas", 20, 1);
+const product9 = new Product (9,"./img/product_bubble-candle.jpg", "Vela burbuja", 700, "velas", 22, 1);
 
 productsCatalog = [];
 productsCatalog.push(product1, product2, product3, product4, product5, product6, product7, product8, product9);
 
-// for (const products of productsCatalog) {
-//   let productCard = document.createElement("article");
-//   productCard.innerHTML = 
-//     `<article class="product-card">
-//       <img class="product-card__img" src="./img/product_soy-candle.jpg" alt="Vela de soja en cuenco de madera">
-//       <h2 class="product-card__title">${products.name}</h2>
-//       <p class="product-card__description">Vela hecha a base de soja, en cuenco de madera. Variedad de aromas.</p>
-//       <p class="product-card__price"> $${products.price} </p>
-//     </article>`
-//   document.querySelector("#main-container").append(productCard);
-// }
-
-function createBuyButton (product) {
-  const button = document.createElement("button");
-  button.classList.add("buy-btn")
-  button.innerText = "Añadir al carrito";
-  button.addEventListener("click", ()=>{
-    addToCart(product);
-    showInTable(product);
-  })
-  return button;
-}
-
 function showProducts (products) {
-  // const productsContainer = document.querySelector("main-container");
-  // productsContainer.innerHTML = "";
+  document.querySelector("#main-container").innerHTML = "";
   productsCatalog.forEach(product => {
     const productCard = document.createElement("article");
     productCard.classList.add("product-card")
@@ -63,9 +40,24 @@ function showProducts (products) {
     const buyButton = createBuyButton(product);
     productCard.append(buyButton);
 
-    document.querySelector("#main-container").prepend(productCard);
+    document.querySelector("#main-container").append(productCard);
+    
   });
 }
+
+function createBuyButton (product) {
+  const button = document.createElement("button");
+  button.classList.add("buy-btn")
+  button.innerText = "Añadir al carrito";
+  button.addEventListener("click", ()=>{
+    showInTable(product);
+    addToCart(product);
+    
+  })
+  return button;
+}
+
+
 
 showProducts();
 
@@ -86,58 +78,139 @@ showProducts();
 
 // FUNCTION
 
-function addToCart (productToAdd) {
-    // if (productToAdd.stock === 0) {
-    //   alert("Producto fuera de stock.")
-    // } 
-    // else if (this.stock < amount){
-    //   alert("La cantidad ingresada supera la disponibilidad que hay en stock de este producto.\nEl stock actual de este producto es de: "+this.stock+" unidades.")
-    // }
-    // else {
-      // for (let i=0 ; i<amount; i++ ) {
-      cart.push(productToAdd);
-      // (productToAdd).stock = --stock;
-      total = total + parseInt((productToAdd).price);
-      productAdded = true; 
-      // }
-    // }    
-}
+
 
 let tableExample = true;
 
 function showInTable(product) {
   
-  if (tableExample === true) {
-    const tableRowExample = document.querySelector("#table-example");
-    document.querySelector("#cart-body").removeChild(tableRowExample);
-    tableExample = false;
-  }
-  const newRow = document.createElement("tr");
-      newRow.classList.add("newRow-product");
-      newRow.innerHTML = `
+  const productExist = cart.includes(product);
+  if (product.stock > 0 && productExist === false ) {
+    const newProductRow = document.createElement("tr");
+      newProductRow.classList.add("newRow-product");
+      newProductRow.innerHTML = `
       <td class="tbody"> ${product.name} </td>
       <td class="tbody"> $${product.price} </td>
-      <td class="tbody"> <input class="number-amount" type="number"> </td>
+      <td class="tbody"> <input id="numberAmount${product.id}" class="number-amount" type="number" min="0" max="${product.stock}"> </td>
       <td class="tbody"> $${parseInt(product.price)} </td>
-      <td class="tbody row-delete"><button class="delete-btn"><i class="fa-solid fa-trash"></i></button></td>
       `
+      const deleteButton = createDeleteButton(product);
+      newProductRow.append(deleteButton);
 
-      document.querySelector("#cart-body").append(newRow);
+      document.querySelector("#cart-body").append(newProductRow);
+  } 
+  else {}
 }
 
+function amountCounter (product) {
+  const amountModifier = document.querySelector(`#numberAmount${product.id}`).value;
+  const productFoundToModify = cart.find(el => el.id === product.id);
+  if (amountModifier <= product.stock) {
+    productFoundToModify.amount = amountModifier;
+    productFoundToModify.stock = productFoundToModify.stock - amountModifier;
+  }
+  else {
+    productFoundToModify.amount = product.stock;
+    document.querySelector(`#numberAmount${product.id}`).value = product.stock;
+  }
+}
+
+// function modifyAmount (product) {
+//   productsCatalog.forEach(product => { 
+//     document.querySelector(`numberAmount${product.id}`).value
+//     });
+
+// }
+
+function addToCart (productToAdd) {
+  if (productToAdd.stock === 0 || productToAdd.amount > productToAdd.stock) {
+    createOutOfStockBanner(productToAdd);
+  } 
+  else {
+  // else if (this.stock < amount){
+  //   alert("La cantidad ingresada supera la disponibilidad que hay en stock de este producto.\nEl stock actual de este producto es de: "+this.stock+" unidades.")
+  // }
+  // else {
+    // for (let i=0 ; i<amount; i++ ) {
+    const productExist = cart.includes(productToAdd);
+    if (productExist === false) {
+    cart.push(productToAdd);
+    document.querySelector(`#numberAmount${productToAdd.id}`).value = 1;
+    console.log(cart);
+    productAdded = true; 
+    }
+    else {
+      const findProductAmount = cart.find(el => el.id === productToAdd.id)
+      findProductAmount.amount++;
+      document.querySelector(`#numberAmount${productToAdd.id}`).value = findProductAmount.amount;
+    }
+  }
+    // }
+  // }    
+}
+   
+
+
+function createOutOfStockBanner (productToAdd) {
+
+  
+  const outOfStockBanner = document.createElement("div");
+    outOfStockBanner.classList.add("out-of-stock-banner-div");
+    outOfStockBanner.innerHTML = `<p class="out-of-stock-banner"> SIN STOCK </p>`
+    const findIndexOfObject = productsCatalog.indexOf(productToAdd);
+    const cards = document.querySelectorAll(".product-card");
+    // cartBody.removeChild(rows[findIndexOfObject]);
+    const cardSelected = cards[findIndexOfObject]
+    cardSelected.prepend(outOfStockBanner);
+    
+  
+}
+
+
+
+function createDeleteButton (product) {
+  const eraseBtn = document.createElement("button");
+  eraseBtn.classList.add("delete-btn")
+  eraseBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`
+  eraseBtn.addEventListener("click", () => {
+    deleteCartProduct(product);
+  })
+  return eraseBtn;
+}
+
+function deleteCartProduct (product) {
+  const findIndexOfObject = cart.indexOf(product);
+  cart.splice(findIndexOfObject, 1);
+  const rows = document.querySelectorAll(".newRow-product");
+  console.log(cart);
+  const cartBody = document.querySelector("#cart-body");
+  cartBody.removeChild(rows[findIndexOfObject]);
+  product.stock = (product.stock)+1;
+}
 
 function showTotalInTable() {
-  // let newRow = document.createElement("tr");
-  //     newRow.innerHTML = ` <tr>
-  //     <td class="theader"> </td>
-  //     <td class="theader"> </td>
-  //     <td class="theader"> </td>
-  //     <td class="theader"> Total: $${total}</td>
-  //     </tr> `
-  //     document.querySelector("#cart-foot").append(newRow);
+  cart.forEach(product => {
+    let totalForProduct = product.amount * product.price;
+    total = total + totalForProduct;
+  });
+
+  let promCode = document.querySelector("#promotionCode").value
+  if (promCode === "ABC12"){
+    descuento = total * 0.2;
+    finalTotal = total - descuento;
+    document.querySelector("#discount").classList.replace("display-hide","display-show")
+    document.querySelector("#discount").innerHTML = `<p class="discount-number"> Descuento: -$${descuento}</p>`
+  }
+  else {
+    finalTotal = total;
+  }
   let totalPrice = document.querySelector("#total-price");
   totalPrice.innerText = ` $${total}`
+
+  document.querySelector("#summarySubtotal").innerText = ` $${finalTotal}`
 }
+
+
 
 // FILTERS
 
@@ -164,8 +237,11 @@ function priceFilter (arr, comparación, valor) {
 
 const cart = []
 
+
 // SHOPPING
 
+let finalTotal = 0;
+let descuento = 0;
 let cantidad = 0;
 let productAdded = false;
 let shopping = true;
@@ -179,97 +255,18 @@ let pay = false;
   // let producto = prompt("Ingrese el nombre del producto que desea agregar al carrito:\nVELA\nJABON LIQUIDO\nDIFUSOR\nSPLASH\nBOLSA AROMATICA\nPara filtrar un producto escriba: FILTRAR\nPara buscar por ID escriba: buscarID\nPara finalizar la compra escriba: FINALIZAR COMPRA");
   
   document.querySelector("#finish-btn").addEventListener("click", ()=> {
-    while (pay === false) {
+    
     console.log("Valor total de la compra: "+total);
+    cart.forEach(product => {
+      amountCounter (product);
+    });
     showTotalInTable();
     document.querySelector("#cart-greeting").innerText = "¡Gracias por tu compra! Esperamos que vuelvas pronto...";
     console.log(cart);
     shopping = false;
     pay = true;
-    }
+    
   }) 
-  
-  // document.querySelector("#buy-btn1").addEventListener("click", ()=>{
-  //   // cantidad = document.querySelector("#buy-amount1").value;
-  //   addToCart(productsCatalog[0]);
-  //   if (productAdded === true){
-  //     showInTable(product1);
-  //     productAdded = false;    
-  //   }
-  // })
-  
-  // document.querySelector("#buy-btn2").addEventListener("click", ()=>{
-  //   // cantidad = document.querySelector("#buy-amount2").value;
-  //   addToCart(productsCatalog[1]);
-  //   if (productAdded === true){
-  //     showInTable(product2);
-  //     productAdded = false;    
-  //   }
-  // })
-
-  // document.querySelector("#buy-btn3").addEventListener("click", ()=>{
-  //   // cantidad = document.querySelector("#buy-amount3").value;
-  //   addToCart(productsCatalog[2]);
-  //   if (productAdded === true){
-  //     showInTable(product4);
-  //     productAdded = false;    
-  //   }
-  // })
-
-  // document.querySelector("#buy-btn4").addEventListener("click", ()=>{
-  //   // cantidad = document.querySelector("#buy-amount4").value;
-  //   addToCart(productsCatalog[3]);
-  //   if (productAdded === true){
-  //     showInTable(product4);
-  //     productAdded = false;    
-  //   }
-  // })
-
-  // document.querySelector("#buy-btn5").addEventListener("click", ()=>{
-  //   cantidad = document.querySelector("#buy-amount5").value;
-  //   addToCart(productsCatalog[4]);
-  //   if (productAdded === true){
-  //     showInTable(product5);
-  //     productAdded = false;    
-  //   }
-  // })
-
-  // document.querySelector("#buy-btn6").addEventListener("click", ()=>{
-  //   cantidad = document.querySelector("#buy-amount6").value;
-  //   addToCart(productsCatalog[5]);
-  //   if (productAdded === true){
-  //     showInTable(product6);
-  //     productAdded = false;    
-  //   }
-  // })
-
-  // document.querySelector("#buy-btn7").addEventListener("click", ()=>{
-  //   cantidad = document.querySelector("#buy-amount7").value;
-  //   addToCart(productsCatalog[6]);
-  //   if (productAdded === true){
-  //     showInTable(product7);
-  //     productAdded = false;    
-  //   }
-  // })
-
-  // document.querySelector("#buy-btn8").addEventListener("click", ()=>{
-  //   cantidad = document.querySelector("#buy-amount8").value;
-  //   addToCart(productsCatalog[7]);
-  //   if (productAdded === true){
-  //     showInTable(product8);
-  //     productAdded = false;    
-  //   }
-  // })
-
-  // document.querySelector("#buy-btn9").addEventListener("click", ()=>{
-  //   cantidad = document.querySelector("#buy-amount9").value;
-  //   addToCart(productsCatalog[8]);
-  //   if (productAdded === true){
-  //     showInTable(product9);
-  //     productAdded = false;    
-  //   }
-  // })
-
   
 //   else if (producto === "filtrar" || producto === "FILTRAR" || producto === "Filtrar") {
 //     let userSelection = parseInt(prompt("Seleccione la opción:\n1) Filtrar productos por categoría.\n2) Filtrar productos por precio."))
@@ -361,73 +358,3 @@ let pay = false;
 // console.log(Cards);
 
 // alert("Su pago ha sido procesado correctamente.\nEl pedido se está preparando... nos pondremos en contacto para coordinar la entrega.");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
