e9582c26434c4c081ffd1bb91c1332e8fd34a58c
Diego Martin Ruiz <ruizdiegomartin.coder@gmail.com>
2022-10-04 14:24:35 -0300

    Padding Fixed

--- js/payment-method/payment-method.js
+++ js/payment-method/payment-method.js
@@ -43,7 +43,7 @@ function selectOneCheck(input){
             }
         })
     }
-}
+};
 
 
 const confirmButton = document.querySelector("#confirmBuyButton");
@@ -54,7 +54,7 @@ function paymentMethodSelect() {
         createBancaryPayDiv(); 
     } 
     else if (card.checked) {
-        window.location.href = "https://ruizdiegomartin.github.io/LetItBe-JavaScript/pages/checkout.html"
+        window.location.href = "https://ruizdiegomartin.github.io/proyecto-final-js/pages/checkout.html"
     }
     else if (cuotas.checked) {
         document.querySelector(".payment-container").classList.add("d-none");
@@ -68,7 +68,7 @@ function paymentMethodSelect() {
                 // SAVE DATA IN LOCALSTORAGE
                 createPaymentObjectInStorage (button)
                 // REDIRECT TO CARD INFO
-                window.location.href = "https://ruizdiegomartin.github.io/LetItBe-JavaScript/pages/checkout.html";
+                window.location.href = "https://ruizdiegomartin.github.io/proyecto-final-js/pages/checkout.html";
             })
         });
     }
@@ -92,7 +92,7 @@ function createBancaryPayDiv (){
     bancaryPay.classList.add("bancary-pay-div");
     bancaryPay.innerHTML = `<h3> Pedido procesado <i class="fa-solid fa-circle-check"></i></h3> <p> <span>¡Gracias por elegirnos!</span><br> Tu pedido fue procesado correctamente y será preparado una vez que se efectivice la transferencia bancaria. Luego, recibirás un correo electrónico explicando los pasos a seguir.</p>`;
     document.querySelector("main").append(bancaryPay);
-    setTimeout( function() { clearStorage(); window.location.href = "https://ruizdiegomartin.github.io/LetItBe-JavaScript/index.html";}, 5000 );
+    setTimeout( function() { clearStorage(); window.location.href = "https://ruizdiegomartin.github.io/proyecto-final-js/index.html";}, 5000 );
 }
 
 function clearStorage (){
